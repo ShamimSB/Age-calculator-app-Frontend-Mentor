8b6d62beafb8e0cadb4ab36f1cc30428ec7a5b40
Shamim Bhuiyan <bhuiyanshamim09@gmail.com>
2026-05-15 08:44:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const btn = document.getElementById('calc-btn');
+
+btn.addEventListener('click', () => {
+    const d1 = parseInt(document.getElementById('day').value);
+    const m1 = parseInt(document.getElementById('month').value);
+    const y1 = parseInt(document.getElementById('year').value);
+
+    const date = new Date();
+    let d2 = date.getDate();
+    let m2 = 1 + date.getMonth();
+    let y2 = date.getFullYear();
+    
+    const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if ((y2 % 4 === 0 && y2 % 100 !== 0) || (y2 % 400 === 0)) {
+        monthDays[1] = 29;
+    }
+
+    if (d1 > d2) {
+        d2 = d2 + monthDays[m2 - 2]; 
+        m2 = m2 - 1;
+    }
+
+    if (m1 > m2) {
+        m2 = m2 + 12;
+        y2 = y2 - 1;
+    }
+
+    const d = d2 - d1;
+    const m = m2 - m1;
+    const y = y2 - y1;
+
+    document.getElementById('out-years').innerText = y;
+    document.getElementById('out-months').innerText = m;
+    document.getElementById('out-days').innerText = d;
+});
